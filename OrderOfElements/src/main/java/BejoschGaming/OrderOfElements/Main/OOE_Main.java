ce6585ad14aac12d4e4065f489b25cee09d67a34
BEJOSCHR <benjamin@schranni.de>
2022-02-21 13:01:27 +0100

    Init eclipse maven project

---
+++ OrderOfElements/src/main/java/BejoschGaming/OrderOfElements/Main/OOE_Main.java
@@ -0,0 +1,11 @@
+package BejoschGaming.OrderOfElements.Main;
+
+public class OOE_Main {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Starting Order of Elements - OOE");
+		
+	}
+
+}

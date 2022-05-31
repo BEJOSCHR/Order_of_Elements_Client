fd30d95bcd9e2ae3b1af5eea7a92204c36110635
BEJOSCHR <benjamin@schranni.de>
2022-06-01 01:56:16 +0200

    Animationsystem basics done, so loading, fade, text and dynamicinteger should be working, not fully tested!

--- OrderOfElements/src/main/java/de/bejoschgaming/orderofelements/animationsystem/AnimationHandler.java
+++ OrderOfElements/src/main/java/de/bejoschgaming/orderofelements/animationsystem/AnimationHandler.java
@@ -4,10 +4,15 @@ import java.util.ArrayList;
 import java.util.List;
 
 import de.bejoschgaming.orderofelements.animationsystem.animations.Animation;
+import de.bejoschgaming.orderofelements.animationsystem.animations.FadeAnimation;
+import de.bejoschgaming.orderofelements.animationsystem.animations.FadeType;
+import de.bejoschgaming.orderofelements.animationsystem.animations.LoadingAnimation;
 
 public class AnimationHandler {
 
 	private static List<Animation> runningAnimations = new ArrayList<Animation>();
+
+	private static LoadingAnimation loadingAnimation = null;
 	
 	public static void startAnimation(Animation animation) {
 		
@@ -16,10 +21,14 @@ public class AnimationHandler {
 		
 	}
 	
-	public static void stopAnimation(Animation animation) {
+	public static void stopAnimation(Animation animation) { stopAnimation(animation, false); } //STOPPED FROM EXTERN SO CALL ANIMATION STOP
+	public static void stopAnimation(Animation animation, boolean fromAnimation) { //CALLED FROM ANIMATION SO DONT CALL ANIMATION TO PREVENT INFINIT-LOOPS
 		
-		animation.finish(false);
-		runningAnimations.remove(animation);
+		if(fromAnimation == false) {
+			animation.finish(false);
+		}else {
+			runningAnimations.remove(animation);
+		}
 		
 	}
 
@@ -28,6 +37,48 @@ public class AnimationHandler {
 		for(Animation animation : runningAnimations) {
 			stopAnimation(animation);
 		}
+		
+	}
+	
+	public static void startLoadingAnimation(String messageToDisplay, int textSize, boolean darkBackground) { startLoadingAnimation(-1, messageToDisplay, textSize, darkBackground); } //INFINIT LOADING UNTIL STOP IS CALLED
+	public static void startLoadingAnimation(int autoFinishAfterThisSteps, String messageToDisplay, int textSize, boolean darkBackground) { //END AFTER THIS STEPS
+		
+		if(loadingAnimation != null) { return; }
+		
+		if(darkBackground == true) {
+			//WITH FADE
+			FadeAnimation fadeAnimation = new FadeAnimation(55, 10, FadeType.FADEIN) {
+				@Override
+				protected void halfTimeAction() {
+					loadingAnimation = new LoadingAnimation(autoFinishAfterThisSteps, messageToDisplay, textSize, darkBackground);
+					AnimationHandler.startAnimation(loadingAnimation);
+				}
+			};
+			AnimationHandler.startAnimation(fadeAnimation);
+		}else {
+			//NO FADE
+			loadingAnimation = new LoadingAnimation(autoFinishAfterThisSteps, messageToDisplay, textSize, darkBackground);
+			AnimationHandler.startAnimation(loadingAnimation);
+		}
+		
+	}
+	public static void updateLoadingMessage(String newMessage) {
+		
+		if(loadingAnimation == null) { return; }
+		
+		loadingAnimation.updateLoadingMessage(newMessage);
+		
+	}
+	public static void stopLoadingAnimation() {
+		
+		if(loadingAnimation == null) { return; };
+		
+		if(loadingAnimation.isDarkBackground() == true) {
+			//WITH FADE
+			AnimationHandler.startAnimation(new FadeAnimation(55, 10, FadeType.FADEOUT));
+		}
+		AnimationHandler.stopAnimation(loadingAnimation);
+		loadingAnimation = null;
 		
 	}
 	
--- OrderOfElements/src/main/java/de/bejoschgaming/orderofelements/animationsystem/animations/DynamicInteger.java
+++ OrderOfElements/src/main/java/de/bejoschgaming/orderofelements/animationsystem/animations/DynamicInteger.java
@@ -1,5 +1,59 @@
 package de.bejoschgaming.orderofelements.animationsystem.animations;
 
-public class DynamicInteger {
+import de.bejoschgaming.orderofelements.animationsystem.AnimationHandler;
 
+public class DynamicInteger extends Animation {
+
+	protected int startValue, endValue;
+	protected int addPerStep;
+	protected int value = 0;
+	
+	public DynamicInteger(int ticksPerStep, int addPerStep, int startValue, int endValue) {
+		super(ticksPerStep, -1);
+		
+		this.startValue = startValue;
+		this.endValue = endValue;
+		this.addPerStep = addPerStep;
+		
+		AnimationHandler.startAnimation(this);
+		
+	}
+
+	@Override
+	protected void startAction() {
+		
+		this.value = this.startValue;
+		
+	}
+	
+	@Override
+	protected void stepAction() {
+		
+		this.value += this.addPerStep;
+		
+		if(this.startValue <= this.endValue) {
+			//POS STEPS (INCREASE)
+			if(this.value >= this.endValue) {
+				this.finish(false);
+			}
+		}else {
+			//NEG STEPS (DECREASE)
+			if(this.value <= this.endValue) {
+				this.finish(false);
+			}
+		}
+		
+	}
+	
+	@Override
+	protected void finishAction(boolean stepLimitReached) {
+		
+		this.value = this.endValue;
+		
+	}
+	
+	public int getValue() {
+		return this.value;
+	}
+	
 }
